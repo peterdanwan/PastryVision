46b761022500670370e9dc7faddb3349eb4dd047
peterdanwan <peterdanwan@gmail.com>
2024-11-19 13:38:39 -0500

    feat: add react frontend made via vite

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,9 @@
+const Card = (children) => {
+  return (
+    <div className={`rounded-lg border bg-card text-card-foreground shadow-sm`}>
+      {children}
+    </div>
+  );
+};
+
+export default Card;
